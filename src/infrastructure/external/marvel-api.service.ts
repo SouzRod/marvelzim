2706ca284bb6ea78553b3f4d0e6a480e0b851a46
Rodrigo Souza <rdrg.rcs1997@gmail.com>
2025-06-29 03:18:52 -0300

    remove comments

--- src/infrastructure/external/marvel-api.service.ts
+++ src/infrastructure/external/marvel-api.service.ts
@@ -2,11 +2,6 @@ import { HttpService } from "@nestjs/axios";
 import { firstValueFrom } from "rxjs/internal/firstValueFrom";
 
 export class MarvelApiService {
-  // private readonly baseUrl: string = process.env.MARVEL_BASE_URL as string;
-  // private readonly apiKey: string = process.env.MARVEL_API_KEY as string;
-  // private readonly hash: string = process.env.MARVEL_HASH as string;
-  // private readonly ts: string = process.env.MARVEL_TS as string;
-
   constructor(
     private readonly httpService: HttpService,
     private readonly apiKey: string,
